d356869291f6bd6c43797199a2fc8fa221cd887f
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-23 20:47:21 +0900

    factory botとpry-railsの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      binding.pry
+    end
+  end
 end
