f4e71e0bee6034b81488870e8dfa91aa438ca6f1
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-25 17:09:53 +0900

    正常系テストの実施

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmation、birthdayが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上であれば登録できる" do
+        @user.password = "abcd123"
+        @user.password_confirmation = "abcd123"
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
     it 'nicknameが空だと登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -102,4 +113,5 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
+end
 end
