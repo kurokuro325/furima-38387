5adc1bf104277ab985fc6903bee927a990d67183
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-26 16:03:43 +0900

    ActiveHashの導入2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :detail_category
+  belongs_to :detail_condition
+  belongs_to :delivery_burden
+  belongs_to :prefecture
+  belongs_to :delivery_day
 end
