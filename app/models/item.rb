ceec0ede28553d1ac2105f2677f67e2900517830
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-26 17:35:03 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,20 @@ class Item < ApplicationRecord
   belongs_to :delivery_burden
   belongs_to :prefecture
   belongs_to :delivery_day
+
+  validates :image, presence: true
+  validates :item, presence: true
+  validates :explanation, presence: true
+  validates :detail_category_id, presence: true
+  validates :detail_condition_id, presence: true
+  validates :delivery_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' },format: { with: /\A[0-9]+\z/ }
+
+  validates :detail_category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :detail_condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_burden_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
