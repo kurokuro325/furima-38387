8b720547bf73ef27d553ee5f7cbeed88251c6e81
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-27 18:33:51 +0900

    入力時にイベントを発火できるようにする

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,6 +2,9 @@ window.addEventListener('load', () => {
 
 const priceInput = document.getElementById("item-price");
 console.log(priceInput);
+priceInput.addEventListener("input", () => {
+  console.log("イベント発火");
+})
 
 const priceInput2 = document.getElementById("add-tax-price");
 console.log(priceInput2);
@@ -10,4 +13,5 @@ const priceInput3 = document.getElementById("profit");
 console.log(priceInput3);
 
   console.log("OK");
+
 });
