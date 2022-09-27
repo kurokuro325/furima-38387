dc8aef84b74a57a736ad4b98fd73b2d0de1df654
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-27 18:26:40 +0900

    必要な要素を取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
+
+const priceInput = document.getElementById("item-price");
+console.log(priceInput);
+
+const priceInput2 = document.getElementById("add-tax-price");
+console.log(priceInput2);
+
+const priceInput3 = document.getElementById("profit");
+console.log(priceInput3);
+
   console.log("OK");
 });
