7c69a3066ad7d789eac8e379e9c384bd240c77fc
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-27 20:54:36 +0900

    HTMLをJavaScriptで作成、ブラウザ上に描画

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,14 +5,14 @@ console.log(priceInput);
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   console.log(inputValue);
-})
 
-const priceInput2 = document.getElementById("add-tax-price");
+  const priceInput2 = document.getElementById("add-tax-price");
 console.log(priceInput2);
+priceInput2.innerHTML = (Math.floor(inputValue * 0.1));
 
 const priceInput3 = document.getElementById("profit");
 console.log(priceInput3);
-
-  console.log("OK");
+priceInput3.innerHTML = (Math.floor(inputValue - priceInput2.innerHTML));
+})
 
 });
