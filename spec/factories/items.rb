ce1ad546161650cc1be364b964dcab0c154cffeb
kurokuro325 <katapa.zari.123@gmail.com>
2022-09-28 15:22:30 +0900

    モデルの単体テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    id            {1}
+    item          { 'abcd123' }
+    explanation   {'Faker::Lorem.sentence'}
+    detail_category_id {2}
+    detail_condition_id {2}
+    delivery_burden_id {2}
+    prefecture_id {2}
+    delivery_day_id {2}
+    price {'3000'}
+    user_id {1}
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/design.jpg'), filename: 'design.jpg')
+    end
   end
 end
