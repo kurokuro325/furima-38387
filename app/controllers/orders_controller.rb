ee1528e185164fe7e4e06d5a0d7507c72f95629c
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-03 17:34:57 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,28 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+       @order_address.save
+       redirect_to items_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address1, :address2, :number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item, :user, :post_code, :prefecture_id, :city, :address1, :address2, :number, :order
+  attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :address1, :address2, :number, :order_id
 
   with_options presence: true do
-  validates :item
-  validates :user
+  validates :item_id
+  validates :user_id
   validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :post_code
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -14,7 +14,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(item: item, user: user)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address1: address1, address2: address2, number: number, order_id: order.id)
   end
 end
