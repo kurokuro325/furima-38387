0a9d1fd7bc573e07468cf978d01491cb4cc721a7
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-03 17:54:06 +0900

    エラーの表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,6 @@ class OrderAddress
   validates :item_id
   validates :user_id
   validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :post_code
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city
   validates :address1
