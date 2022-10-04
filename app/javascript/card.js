af3ee6c846b3f8b913e32cd10882df6db32772c2
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-04 16:21:57 +0900

    トークンをサーバーサイドへ送る2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -21,6 +21,12 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      document.getElementById("order_card_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
