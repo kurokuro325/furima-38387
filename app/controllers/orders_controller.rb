9701bfa045400065c695217163f1d0ca3d0ba069
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-04 17:45:15 +0900

    ストロングパラメーターの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,7 +23,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address1, :address2, :number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address1, :address2, :number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
 end
