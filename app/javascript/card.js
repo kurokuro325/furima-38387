c8655ba3f18fd8e7bd2b1aaf64d986ba523e4219
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-04 20:56:00 +0900

    鍵情報を環境変数として設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_a8823e0a39e308d6ab064bc3");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
