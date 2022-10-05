12115427c35abe027b1bb9361f46d7e960b06977
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-05 20:35:34 +0900

    ログアウト状態、ログインページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
