62b671cd70102368b11e76e9df078cc009fab01d
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-05 15:23:06 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :order
-  
+
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
     user_id { 2 }
     post_code { '440-8975' }
     prefecture_id { 3 }
-    city {'横浜市緑区'}
-    address1 {'青山2-8-6'}
-    number {'09044569865'}
-    token {"tok_abcdefghijk00000000000000000"}
+    city { '横浜市緑区' }
+    address1 { '青山2-8-6' }
+    number { '09044569865' }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
